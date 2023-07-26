89ff1eb406f7b72c576ecf02afff3a741a97b3db
Jackie Chen <jackie@Jackies-MacBook-Air.local>
2023-07-26 20:49:18 +0800

    test: try github action deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainPage from "./components/MainPage";
+// import MainPage from "./components/MainPage";
 import AboutPage from "./components/AboutPage";
 import ScrollToTop from "./components/SrollToTop";
 
@@ -10,8 +10,8 @@ function App() {
       <BrowserRouter>
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<MainPage />}></Route>
-          <Route path="/aboutPage" element={<AboutPage />}></Route>
+          {/* <Route path="/" element={<MainPage />}></Route> */}
+          <Route path="/" element={<AboutPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
